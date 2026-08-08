26f50a5227c83f233c66ef4809372560712daccf
Enrico Hoffmann <enricohoffmann@web.de>
2026-08-08 11:48:46 +0200

    feat(home-page): enhance layout with hero section and button component; add font files

--- src/app/features/home-page/home-page.ts
+++ src/app/features/home-page/home-page.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HeaderComponent } from "../../layout/header/header-component/header-component";
 import { CallToAction } from '../../shared/components/call-to-action/call-to-action';
 import { FooterComponent } from '../../layout/footer/footer-component/footer-component';
+import { ButtonComponent } from '../../shared/components/button-component/button-component';
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderComponent, CallToAction, FooterComponent],
+  imports: [HeaderComponent, CallToAction, FooterComponent, ButtonComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
-export class HomePage {
+export class HomePage  {
   readonly heroImages = [
     '/assets/img/hero-dish-01.webp',
     '/assets/img/hero-dish-02.webp',
@@ -18,4 +19,7 @@ export class HomePage {
     '/assets/img/hero-dish-04.webp',
     '/assets/img/hero-dish-05.webp',
   ];
+
+
+
 }
