7e87a572d8f78c1060f3407ed9c426749c3098a1
Enrico Hoffmann <enricohoffmann@web.de>
2026-08-08 09:12:15 +0200

    feat(home-page): add hero images and restructure layout with footer and call-to-action components

--- src/app/features/home-page/home-page.ts
+++ src/app/features/home-page/home-page.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../layout/header/header-component/header-component";
+import { CallToAction } from '../../shared/components/call-to-action/call-to-action';
+import { FooterComponent } from '../../layout/footer/footer-component/footer-component';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CallToAction, FooterComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
-export class HomePage {}
+export class HomePage {
+  readonly heroImages = [
+    '/assets/img/hero-dish-01.webp',
+    '/assets/img/hero-dish-02.webp',
+    '/assets/img/hero-dish-03.webp',
+    '/assets/img/hero-dish-04.webp',
+    '/assets/img/hero-dish-05.webp',
+  ];
+}
