deb68ba8b4c7db4de6302730ec13934a0c4ed50e
Enrico Hoffmann <enricohoffmann@web.de>
2026-08-12 14:00:14 +0200

    feat(call-to-action): integrate Angular CDK for responsive design; enhance button logic for mobile compatibility

--- src/app/shared/components/call-to-action/call-to-action.ts
+++ src/app/shared/components/call-to-action/call-to-action.ts
@@ -1,6 +1,9 @@
-import { Component, input, signal, output } from '@angular/core';
+import { Component, input, signal, output, inject } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { ColorVariant, CTAVariant } from '../../utils/types';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-call-to-action',
@@ -14,6 +17,13 @@ export class CallToAction {
   actionButtonEvent = output<void>();
   callToActionVariant = input<CTAVariant>('forward');
   colorVariant = input<ColorVariant>('white');
+  private readonly breakpointObserver = inject(BreakpointObserver);
+
+  readonly isMobile = toSignal(
+    this.breakpointObserver.observe('(max-width: 650px)').pipe(
+      map(result => result.matches)
+    ), {initialValue: false}
+  );
 
   onPointerEnter(): void {
     this.animationState.set('enter');
